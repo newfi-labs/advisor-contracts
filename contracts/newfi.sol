b63722818c11f6035713f82b9ef2a36f8e4f05dd
Viraz Malhotra <virajm72@gmail.com>
2020-10-06 06:05:30 +0530

    refactor(newfi contract): added propotion tracking and replaced two pools with one common pool (#1)

--- contracts/newfi.sol
+++ contracts/newfi.sol
@@ -2,20 +2,21 @@ pragma solidity 0.6.2;
 
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/ERC20.sol";
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/SafeERC20.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/utils/ReentrancyGuard.sol";
 
-
-contract NewFi {
+contract NewF is ReentrancyGuard {
     using SafeERC20 for ERC20;
     using SafeMath for uint;
-    event AdvisorOnBoarded(string name, uint256 tradingExpereience,  string twiiterProfile, address stablePool, address volatilePool,   uint256 stakedAmount);
-       
+
+    event AdvisorOnBoarded(string name, uint256 tradingExpereience,  string twiiterProfile, address pool, uint256 stakedAmount);
+    
+    event Investment(uint256 _stablecoinAmount, address _advisor, address _poolAddress, uint256 _stablePropotion, uint256 _volatilePropotion);
     
     struct Advisor {
         string naame;
         uint256 tradingExpereience;
         string twiiterProfile;
-        address stablePool;
-        address volatilePool;
+        address pool;
         uint256 stakedAmount;
     }
     
@@ -23,6 +24,8 @@ contract NewFi {
         uint256 stablePoolLiquidity;
         uint256 volatilePoolLiquidity;
         uint256 poolTokenBalance;
+        uint256 stablePoolPropotion;
+        uint256 volatilePoolPropotion;
         bool status;
     }
     
@@ -41,9 +44,9 @@ contract NewFi {
         @param _twiiterProfile Twitter username of the advisor.
      */
     function onboard(string calldata _name, uint256 _tradingExpereience, string calldata _twiiterProfile) external {
-        advisorInfo[msg.sender] = Advisor(_name, _tradingExpereience, _twiiterProfile, address(0), address(0), 0);
+        advisorInfo[msg.sender] = Advisor(_name, _tradingExpereience, _twiiterProfile, address(0), 0);
         advisors.push(msg.sender);
-        emit AdvisorOnBoarded(_name, _tradingExpereience, _twiiterProfile, address(0), address(0), 0);
+        emit AdvisorOnBoarded(_name, _tradingExpereience, _twiiterProfile, address(0), 0);
     }
     
     /**
@@ -51,23 +54,24 @@ contract NewFi {
         @param _stablecoin address of stablecoin.
         @param _stablecoinAmount amount of stable coin.
         @param _advisor address os selected advisorr.
-        @param _stablePoolAddress address of advisor's stable pool to be created by gnosis sdk.
-        @param _volatilePoolAddress address of the advisor's volatile pool to be created by gnosis sdk.
+        @param _poolAddress address of advisor's pool to be created by gnosis sdk.
+        @param _stablePropotion stable coin propotion used to invest in protocols.
+        @param _volatilePropotion stable coin propotion used to invest in protocols.
      */
-    function invest(address _stablecoin, uint _stablecoinAmount, address _advisor, address _stablePoolAddress, address _volatilePoolAddress) payable external {
-        ERC20(_stablecoin).safeTransferFrom(address(this), _stablePoolAddress, _stablecoinAmount);
-        (bool success, ) = _volatilePoolAddress.call.value(msg.value)("");
+    function invest(address _stablecoin, uint256 _stablecoinAmount, address _advisor, address _poolAddress, uint256 _stablePropotion, uint256 _volatilePropotion) payable nonReentrant external {
+        ERC20(_stablecoin).safeTransferFrom(address(this), _poolAddress, _stablecoinAmount);
+        (bool success, ) = _poolAddress.call.value(msg.value)("");
         require(success, "Transfer failed.");
         Advisor storage advisor = advisorInfo[_advisor];
-        advisor.stablePool = _stablePoolAddress;
-        advisor.volatilePool = _volatilePoolAddress;
+        advisor.pool = _poolAddress;
         Investor storage investor = investorInfo[msg.sender];
         if (investor.status) {
             investor.stablePoolLiquidity = investor.stablePoolLiquidity.add(_stablecoinAmount);
             investor.volatilePoolLiquidity = investor.volatilePoolLiquidity.add(msg.value);
             // not including the pool token balance calculation at the moment
         } else {
-            investorInfo[msg.sender] = Investor(_stablecoinAmount, msg.value, 0, true);
+            investorInfo[msg.sender] = Investor(_stablecoinAmount, msg.value, 0, _stablePropotion, _volatilePropotion, true);
         }
+        emit Investment(_stablecoinAmount, _advisor, _poolAddress, _stablePropotion, _volatilePropotion);
     }
 }
