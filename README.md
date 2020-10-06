bd51e0fce1ff147c02a5c2fa407176b1791933de
Herman (Boomer) Rogers <herman.rogers@pm.me>
2020-10-05 23:34:59 -0400

    Add init for open zeppelin as build system (#2)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # advisor-contracts
 Contracts for the advisor/investor portal
+
+## Getting started
+
+`npm install` - install project dependencies
+
+`npx oz compile` - compile needed contracts
--- contracts/newfi.sol
+++ contracts/newfi.sol
@@ -1,21 +1,22 @@
-pragma solidity 0.6.2;
+pragma solidity ^0.6.2;
 
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/ERC20.sol";
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/SafeERC20.sol";
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/utils/ReentrancyGuard.sol";
 
-contract NewF is ReentrancyGuard {
-    using SafeERC20 for ERC20;
+contract NewFi is ReentrancyGuardUpgradeSafe {
+    using SafeERC20 for IERC20;
     using SafeMath for uint;
 
-    event AdvisorOnBoarded(string name, uint256 tradingExpereience,  string twiiterProfile, address pool, uint256 stakedAmount);
-    
-    event Investment(uint256 _stablecoinAmount, address _advisor, address _poolAddress, uint256 _stablePropotion, uint256 _volatilePropotion);
-    
-    struct Advisor {
-        string naame;
-        uint256 tradingExpereience;
-        string twiiterProfile;
+    event AdvisorOnBoarded(string name, uint256 tradingExperience,  string twitterProfile, address pool,  uint256 stakedAmount);
+
+    event Investment(uint256 _stablecoinAmount, address _advisor, address _poolAddress, uint256 _stableProportion, uint256 _volatileProportion);
+
+
+struct Advisor {
+        string name;
+        uint256 tradingExperience;
+        string twitterProfile;
         address pool;
         uint256 stakedAmount;
     }
@@ -24,8 +25,8 @@ contract NewF is ReentrancyGuard {
         uint256 stablePoolLiquidity;
         uint256 volatilePoolLiquidity;
         uint256 poolTokenBalance;
-        uint256 stablePoolPropotion;
-        uint256 volatilePoolPropotion;
+        uint256 stablePoolProportion;
+        uint256 volatilePoolProportion;
         bool status;
     }
     
@@ -40,13 +41,13 @@ contract NewF is ReentrancyGuard {
     /**
         Onboards a new Advisor
         @param _name Name of the Advisor.
-        @param _tradingExpereience Trading exp in years for the investors in help chossing a particular advisor.
-        @param _twiiterProfile Twitter username of the advisor.
+        @param _tradingExperience Trading exp in years for the investors in help choosing a particular advisor.
+        @param _twitterProfile Twitter username of the advisor.
      */
-    function onboard(string calldata _name, uint256 _tradingExpereience, string calldata _twiiterProfile) external {
-        advisorInfo[msg.sender] = Advisor(_name, _tradingExpereience, _twiiterProfile, address(0), 0);
+    function onboard(string calldata _name, uint256 _tradingExperience, string calldata _twitterProfile) external {
+        advisorInfo[msg.sender] = Advisor(_name, _tradingExperience, _twitterProfile, address(0), 0);
         advisors.push(msg.sender);
-        emit AdvisorOnBoarded(_name, _tradingExpereience, _twiiterProfile, address(0), 0);
+        emit AdvisorOnBoarded(_name, _tradingExperience, _twitterProfile, address(0), 0);
     }
     
     /**
@@ -55,23 +56,26 @@ contract NewF is ReentrancyGuard {
         @param _stablecoinAmount amount of stable coin.
         @param _advisor address os selected advisorr.
         @param _poolAddress address of advisor's pool to be created by gnosis sdk.
-        @param _stablePropotion stable coin propotion used to invest in protocols.
-        @param _volatilePropotion stable coin propotion used to invest in protocols.
+        @param _stableProportion stable coin proportion used to invest in protocols.
+        @param _volatileProportion stable coin proportion used to invest in protocols.
+        @param _advisor address os selected advisor.
      */
-    function invest(address _stablecoin, uint256 _stablecoinAmount, address _advisor, address _poolAddress, uint256 _stablePropotion, uint256 _volatilePropotion) payable nonReentrant external {
-        ERC20(_stablecoin).safeTransferFrom(address(this), _poolAddress, _stablecoinAmount);
-        (bool success, ) = _poolAddress.call.value(msg.value)("");
+    function invest(address _stablecoin, uint256 _stablecoinAmount, address _advisor, address _poolAddress, uint256 _stableProportion, uint256 _volatileProportion) payable nonReentrant external {
+        IERC20(_stablecoin).safeTransferFrom(address(this), _poolAddress, _stablecoinAmount);
+        (bool success, ) = _poolAddress.call{value:msg.value}("");
         require(success, "Transfer failed.");
+
         Advisor storage advisor = advisorInfo[_advisor];
         advisor.pool = _poolAddress;
         Investor storage investor = investorInfo[msg.sender];
+
         if (investor.status) {
             investor.stablePoolLiquidity = investor.stablePoolLiquidity.add(_stablecoinAmount);
             investor.volatilePoolLiquidity = investor.volatilePoolLiquidity.add(msg.value);
             // not including the pool token balance calculation at the moment
         } else {
-            investorInfo[msg.sender] = Investor(_stablecoinAmount, msg.value, 0, _stablePropotion, _volatilePropotion, true);
+            investorInfo[msg.sender] = Investor(_stablecoinAmount, msg.value, 0, _stableProportion, _volatileProportion, true);
         }
-        emit Investment(_stablecoinAmount, _advisor, _poolAddress, _stablePropotion, _volatilePropotion);
+        emit Investment(_stablecoinAmount, _advisor, _poolAddress, _stableProportion, _volatileProportion);
     }
 }
